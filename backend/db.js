f68aa4a8c42d165f350f2058441ca6ade40befae
ovidiu94 <5borzo85@solent.ac.uk>
2022-05-02 15:38:06 +0100

    Second push for fullstack webbapp

--- backend/db.js
+++ backend/db.js
@@ -4,14 +4,16 @@ var mongoURL='mongodb+srv://admin:admin12345@gamestore.xhnru.mongodb.net/game-st
 
 mongoose.connect(mongoURL, {useUnifiedTopology: true, useNewUrlParser: true})
 
+
+
 var db = mongoose.connection
 
-db.on('connected', ()=>{
+db.on('connected' , ()=>{
     console.log('MongoDB Connection Successfull');
 })
 
-db.on('error', ()=>{
-    console.log('MongoDB Connection Failed');
+db.on('error' , ()=>{
+    console.log(`MongoDB Connection failed`);
 })
 
-module.exports = mongoose
+module.exports =mongoose
--- backend/server.js
+++ backend/server.js
@@ -7,28 +7,29 @@ const app = express();
 const db = require('./db.js')
 app.use(express.json());
 
+const path = require('path');
 const gamesRoute = require('./routes/gamesRoute')
 const userRoute = require('./routes/userRoute')
 const ordersRoute = require('./routes/ordersRoute')
 
+
 app.use('/api/games/', gamesRoute);
 app.use('/api/users/', userRoute);
 app.use('/api/orders/', ordersRoute);
-app.get("/", (req, res) => {
-    res.send("Server working 🔥" + port);
-});
 
-// app.get("/getgames", (req, res) => {
-//     Game.find({}, (err , docs)=>{
-        
-//         if(err){
-//             console.log(err);
-//         }
-//         else{
-//             res.send(docs)
-//         }
-//     })
-// })
+
+
+if(process.env.NODE_ENV ==='production')
+{
+    app.use('/' , express.static('frontend/build'))
+
+    app.get('*' , (req , res)=>{
+
+        res.sendFile(path.resolve(__dirname  , 'frontend/build/index.html'))
+
+    })
+}
+
 
 const port =process.env.PORT || 8000;
 
