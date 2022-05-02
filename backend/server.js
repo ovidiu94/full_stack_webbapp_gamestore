3dc00b45c5e2a303ceb4237bb3eb81968fd904d6
ovidiu94 <5borzo85@solent.ac.uk>
2022-05-02 20:19:47 +0100

    Third push for fullstack webbapp

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,11 @@ app.use('/api/orders/', ordersRoute);
 
 if(process.env.NODE_ENV ==='production')
 {
-    app.use('/' , express.static('frontend/build'))
+    app.use('/' , express.static('app/backend/frontend/build'))
 
     app.get('*' , (req , res)=>{
 
-        res.sendFile(path.resolve(__dirname  , 'frontend/build/index.html'))
+        res.sendFile(path.resolve(__dirname  , 'app/backend/frontend/build/index.html'))
 
     })
 }
