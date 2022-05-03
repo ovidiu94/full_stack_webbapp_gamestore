d3610df34cf96c2762f8572385fba08d3d485f08
ovidiu94 <5borzo85@solent.ac.uk>
2022-05-03 01:37:11 +0100

    webbapp updated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+
 const Game = require("./models/gameModel");
 
 const app = express();
@@ -16,23 +17,15 @@ app.use("/api/users/", userRoute);
 app.use("/api/orders/", ordersRoute);
 
 const __dirname = path.resolve()
-// if(process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname,'/frontend/build')))
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
 
-//     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-//     }else {
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+    }else {
 
-//     app.get('/', (req,res) => {
-//     res.send ('Api is running')})
-//     }
-
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("/frontend/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
-  });
-}
+    app.get('/', (req,res) => {
+    res.send ('Api is running')})
+    }
 
 const port = process.env.PORT || 8000;
 
