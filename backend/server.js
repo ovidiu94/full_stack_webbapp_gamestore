3569354f2ae3f59b2a7a5202191507dfed5a7965
ovidiu94 <5borzo85@solent.ac.uk>
2022-05-03 01:33:28 +0100

    webbapp updated

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,39 @@
-const express = require('express');
+const express = require("express");
 
-const Game = require('./models/gameModel')
-
+const Game = require("./models/gameModel");
 
 const app = express();
-const db = require('./db')
+const db = require("./db");
 
-const path = require('path');
-const gamesRoute = require('./routes/gamesRoute')
-const userRoute = require('./routes/userRoute')
-const ordersRoute = require('./routes/ordersRoute')
+const path = require("path");
+const gamesRoute = require("./routes/gamesRoute");
+const userRoute = require("./routes/userRoute");
+const ordersRoute = require("./routes/ordersRoute");
 app.use(express.json());
 
-app.use('/api/games/', gamesRoute);
-app.use('/api/users/', userRoute);
-app.use('/api/orders/', ordersRoute);
+app.use("/api/games/", gamesRoute);
+app.use("/api/users/", userRoute);
+app.use("/api/orders/", ordersRoute);
 
-//const __dirname = path.resolve()
+const __dirname = path.resolve()
 // if(process.env.NODE_ENV === 'production') {
 //     app.use(express.static(path.join(__dirname,'/frontend/build')))
-  
+
 //     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 //     }else {
-   
+
 //     app.get('/', (req,res) => {
 //     res.send ('Api is running')})
 //     }
 
-if(process.env.NODE_ENV ==='production')
-{
-    app.use('/' , express.static('/frontend/build'))
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("/frontend/build"));
 
-    app.get('*' , (req , res)=>{
-
-        res.sendFile(path.resolve(__dirname  , 'frontend/build/index.html'))
-
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
+  });
 }
 
-   
-    const port =process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
-
-app.listen(port, ()=> 'Server running on port port 🔥');
+app.listen(port, () => "Server running on port port 🔥");
