31805fbe6fa66e67d56d20696436bb7ed7443378
ovidiu94 <5borzo85@solent.ac.uk>
2022-05-05 11:35:25 +0100

    webbapp updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
@@ -7,6 +8,14 @@ const userSchema = mongoose.Schema({
     isAdmin : {type: Boolean, require, default: false},
 }, {
     timestamps: true,
+})
+userSchema.pre('save', async function(next){
+    
+    if(!this.isModified('password')) {
+        next()
+    }
+    const salt =await bcrypt.genSalt(15)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 module.exports = mongoose.model('users', userSchema)
